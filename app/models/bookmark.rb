67a53299c774d06a19b7115e655f6a0f8f0b116b
Sebastian Burke <burke.sebastian1@gmail.com>
2024-05-23 21:00:40 +0800

    first day challenge done

--- app/models/bookmark.rb
+++ app/models/bookmark.rb
@@ -3,5 +3,5 @@ class Bookmark < ApplicationRecord
   belongs_to :movie
 
   validates :comment, length: { minimum: 6 }
-  validates :list, :uniqueness => { :scope => :movie}
+  validates :list, :uniqueness => { :scope => :movie }
 end
